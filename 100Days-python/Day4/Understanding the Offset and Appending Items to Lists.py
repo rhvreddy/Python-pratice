e2538672769f78f38fc6897b2d4c4e4860cbb924
harsha <harshavardhanrock007@gmail.com>
2024-09-10 15:19:32 +0530

    latest changes

---
+++ 100Days-python/Day4/Understanding the Offset and Appending Items to Lists.py
@@ -0,0 +1,14 @@
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia"]
+
+print(states_of_america[1]) ## [0] helps us to get the list of the first word
+print(states_of_america[-1]) ## [-1] helps us to get the list if the final word
+
+states_of_america[1] = "Pencilvania" ## This help us to list the word and replace that particular word
+
+states_of_america.append("Angelaland") ## This append helps us to add the country at the end of the list
+
+states_of_america.extend(["Angelalands...", "Jack Bauer"]) ## This extand help us to add the bunch of data at the end of the list
+print(states_of_america)
+
+
+## Documentation url ==> https://docs.python.org/3/tutorial/datastructures.html
