ac7a4082ab8532f40370e56563aff857e521ce3f
harsha <harshavardhanrock007@gmail.com>
2024-09-12 14:51:35 +0530

    latest changes

---
+++ 100Days-python/Day8/Functions with Inputs.py
@@ -0,0 +1,24 @@
+## Functions
+# def my_function():
+#     #Do this
+#     #Then do this
+#     #Finally do this
+
+def greet():
+    print("hello")
+    print("how do you do?")
+    print("Isn't the weather nice?")
+
+greet() ## This function is used to repeat the same instruction several times ==> how many times we repeat the greet word that many times it will print
+greet()
+
+## Functions that allows for inputs
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}?")
+
+greet_with_name("Harsha")
+
+# def my_function(something):
+# something(parameter) = 123(argument)
---
+++ 100Days-python/Day8/Positional vs. keyword Arguments.py
@@ -0,0 +1,8 @@
+## Functions with more than 1 input
+
+def greet_with(name:str, location:str):
+    print(f"Hello {name}.")
+    print(f"What is it like in {location}.")
+
+greet_with("Harsha", "Vizag")
+greet_with(location="hyd", name="viper")
